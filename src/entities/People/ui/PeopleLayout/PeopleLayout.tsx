84c374c6993d42ca0e771ebaed6fc8c76a003a55
Gregory <g.usoltsev.worksrocket@gmail.com>
2024-01-22 16:16:54 +0500

    feat: refactor images

--- src/entities/People/ui/PeopleLayout/PeopleLayout.tsx
+++ src/entities/People/ui/PeopleLayout/PeopleLayout.tsx
@@ -7,17 +7,15 @@ interface PeopleLayoutProps {
   children: React.ReactNode;
 }
 
+import ImageBackground from '~/svg/image-background.svg';
+
 export const PeopleLayout = (props: PeopleLayoutProps): JSX.Element => {
   const { className, children, ...rest } = props;
 
   return (
     <div className={cn(styles.PeopleLayout, className)} {...rest}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        className={styles.PeopleLayout__background}
-        src='./images/background.jpg'
-        alt='background'
-      />
+      <ImageBackground className={styles.PeopleLayout__background} />
       <div className={styles.PeopleLayout__container}>{children}</div>
     </div>
   );
